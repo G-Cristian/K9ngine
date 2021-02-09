334711b924c86d9130085ba65304112cdb031fe0
G-Cristian <G-Cristian@users.noreply.github.com>
2021-02-09 18:28:32 -0300

    Added RenderersContainer

---
+++ K9ngine/K9ngineFramework/RenderersContainer.cpp
@@ -0,0 +1,23 @@
+#include <RenderersContainer.h>
+
+#include <Renderer.h>
+#include <RenderingComponent.h>
+#include <World.h>
+
+#include <memory>
+#include <set>
+
+namespace K9 {
+	
+	namespace Graphics {
+		void RenderersContainer::draw(const World& world, const std::set<std::shared_ptr<const K9::Components::RenderingComponent>>& renderingComponents) const {
+			for (auto it = _renderers.cbegin(); it != _renderers.cend(); ++it) {
+				(*it)->draw(world, renderingComponents);
+			}
+		}
+
+		void RenderersContainer::addRenderer(const std::shared_ptr<const IRenderer>& renderer) {
+			_renderers.push_back(renderer);
+		}
+	}
+}
---
+++ K9ngine/K9ngineFramework/includes/RenderersContainer.h
@@ -0,0 +1,37 @@
+#ifndef RENDERERSCONTAINER_H_
+#define RENDERERSCONTAINER_H_
+
+#include <Renderer.h>
+
+#include <list>
+#include <memory>
+#include <set>
+#include <string>
+
+namespace K9 {
+	namespace Components {
+		class RenderingComponent;
+	}
+}
+
+namespace K9 {
+	class World;
+	namespace Graphics {
+		class RenderersContainer :public IRenderer {
+		public:
+			RenderersContainer(const std::string& name) :
+				IRenderer(name) {
+			}
+
+			virtual ~RenderersContainer() = default;
+
+			void draw(const World& world, const std::set<std::shared_ptr<const K9::Components::RenderingComponent>>& renderingComponents) const override;
+
+			void addRenderer(const std::shared_ptr<const IRenderer>& renderer);
+		private:
+			std::list<const std::shared_ptr<const IRenderer>> _renderers;
+		};
+	}
+}
+
+#endif // !RENDERERSCONTAINER_H_
