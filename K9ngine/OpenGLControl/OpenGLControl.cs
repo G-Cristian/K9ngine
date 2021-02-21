f04503c1f848db7aa390c7a04c58228144eecc38
G-Cristian <G-Cristian@users.noreply.github.com>
2021-02-21 19:24:51 -0300

    Added events and functions for creating buffers and rendering program (NOTE: still not working since I have to implement it in OenGL class)

--- K9ngine/OpenGLControl/OpenGLControl.cs
+++ K9ngine/OpenGLControl/OpenGLControl.cs
@@ -11,11 +11,11 @@ using System.Windows.Forms;
 
 namespace OpenGLControl
 {
-    public partial class OpenGLControl: UserControl
-    {
+	public partial class OpenGLControl : UserControl
+	{
 		#region Members
 
-		private OpenGL _openGLInstance= null;
+		private OpenGL _openGLInstance = null;
 		private int _majorVersion = 0;
 		private int _minorVersion = 0;
 
@@ -24,11 +24,41 @@ namespace OpenGLControl
 		#endregion
 
 		#region Delegates/Events
-		public delegate void RenderDelegate(OpenGL openGLInstance);
 
-		public event RenderDelegate PreRender;
-		public event RenderDelegate Render;
-		public event RenderDelegate PostRender;
+		public class OpenGLControlEventArgs
+		{
+			public OpenGLControlEventArgs(OpenGL openGLInstance)
+			{
+				OpenGLInstanse = openGLInstance;
+			}
+			public OpenGL OpenGLInstanse { get; private set; }
+		}
+
+		public class ErrorEventArgs : EventArgs
+		{
+			public int LastWin32Error { get; private set; }
+			public List<int> OpenGLErrors { get; private set; }
+			public string Msg { get; private set; }
+			public string StackTrace { get; private set; }
+			public ErrorEventArgs(int lastWin32Error, List<int> openGLErrors, string msg, string stackTrace = ""):
+				base()
+			{
+				LastWin32Error = lastWin32Error;
+				OpenGLErrors = openGLErrors;
+				Msg = msg;
+				StackTrace = stackTrace;
+			}
+		}
+
+		public delegate void OpenGLControlDelegate(OpenGLControl sender, OpenGLControlEventArgs eventArgs);
+
+		public event OpenGLControlDelegate OnInitialized;
+
+		public event OpenGLControlDelegate PreRender;
+		public event OpenGLControlDelegate Render;
+		public event OpenGLControlDelegate PostRender;
+
+		public event EventHandler OnError;
 
 		#endregion
 
@@ -69,43 +99,37 @@ namespace OpenGLControl
 
 		#endregion
 
-			#region Control Methods
+		#region Control Methods
 
 		public OpenGLControl()
-        {
-            InitializeComponent();
-        }
+		{
+			InitializeComponent();
+		}
 
 		bool red = true;
 		private void OpenGLControl_Load(object sender, EventArgs e)
 		{
 			if (this.DesignMode) return;
 
-			//OpenGLWrapperClass.LoadLibrary(OpenGLWrapperClass.opengl_dll);
-			//OpenGLWrapperClass.LoadLibrary(OpenGLWrapperClass.glew32_dll);
-			_openGLInstance = new OpenGL(this.Handle, _allowDebug);
-			OpenGL.Settings settings = new OpenGL.Settings();
-			//TODO: Get versions from properties
-			settings.majorVersion = MajorVersion;
-			settings.minorVersion = MinorVersion;
+			try
+			{
+				//OpenGLWrapperClass.LoadLibrary(OpenGLWrapperClass.opengl_dll);
+				//OpenGLWrapperClass.LoadLibrary(OpenGLWrapperClass.glew32_dll);
+				_openGLInstance = new OpenGL(this.Handle, _allowDebug);
+				OpenGL.Settings settings = new OpenGL.Settings();
+				//TODO: Get versions from properties
+				settings.majorVersion = MajorVersion;
+				settings.minorVersion = MinorVersion;
 
-			_openGLInstance.Init(settings);
+				_openGLInstance.Init(settings);
 
 
-			//OpenGLWrapperClass.initGlew();
-			//error = Marshal.GetLastWin32Error();
-
-			//OpenGLWrapperClass.clearColorBuffer();
-			//error = Marshal.GetLastWin32Error();
-
-			//OpenGLWrapperClass.SwapBuffers(hDC);
-			//error = Marshal.GetLastWin32Error();
-
-			//OpenGLWrapperClass.setFrontFaceClockwise();
-
-			//string error = OpenGLWrapperClass.GetOpenGLError(1);
-
-			//MessageBox.Show(error);
+				RiseOpenGLEvent(OnInitialized);
+			}
+			catch(OpenGL.OpenGLException openGLException)
+			{
+				RiseErrorEvent(openGLException);
+			}
 
 		}
 
@@ -114,7 +138,7 @@ namespace OpenGLControl
 			if (DesignMode) {
 				return;
 			}
-			
+
 			//IntPtr hdc = e.Graphics.GetHdc();
 			//error = Marshal.GetLastWin32Error();
 
@@ -132,22 +156,53 @@ namespace OpenGLControl
 
 		#region Custom Methods
 
+		public uint CreateAndBindVertexArrayObject()
+		{
+			try
+			{
+				return _openGLInstance.CreateAndBindVertexArrayObject();
+			}
+			catch(OpenGL.OpenGLException e)
+			{
+				RiseErrorEvent(e);
+
+				return 0;
+			}
+		}
+
+		public uint CreateArrayBuffer(float[] attributes)
+		{
+			try
+			{
+				return _openGLInstance.CreateArrayBuffer(attributes);
+			}
+			catch (OpenGL.OpenGLException e)
+			{
+				RiseErrorEvent(e);
+
+				return 0;
+			}
+		}
+
+		public uint CreateElementArrayBuffer(int[] attributes)
+		{
+			try
+			{
+				return _openGLInstance.CreateElementArrayBuffer(attributes);
+			}
+			catch (OpenGL.OpenGLException e)
+			{
+				RiseErrorEvent(e);
+
+				return 0;
+			}
+		}
+
 		public void DrawScene()
 		{
-			if(PreRender != null)
-			{
-				PreRender(_openGLInstance);
-			}
-
-			if(Render != null)
-			{
-				Render(_openGLInstance);
-			}
-
-			if (PostRender != null)
-			{
-				PostRender(_openGLInstance);
-			}
+			RiseOpenGLEvent(PreRender);
+			RiseOpenGLEvent(Render);
+			RiseOpenGLEvent(PostRender);
 
 			try
 			{
@@ -155,21 +210,42 @@ namespace OpenGLControl
 				// so I throw an exception
 				_openGLInstance.SwapBuffers();
 			}
-			catch(Exception e)
+			catch (OpenGL.OpenGLException e)
 			{
-				if (_allowDebug)
-				{
-					Console.WriteLine(e.Message);
-				}
+				RiseErrorEvent(e);
 			}
 		}
-
-		
 
 		#endregion
 
 		#region Helper Methods
-		
+
+		private void RiseOpenGLEvent(OpenGLControlDelegate openGLEvent)
+		{
+			OpenGLControlEventArgs eventArgs = new OpenGLControlEventArgs(_openGLInstance);
+			if (openGLEvent != null)
+			{
+				Invoke(openGLEvent, this, eventArgs);
+			}
+		}
+
+		private void RiseErrorEvent(ErrorEventArgs eventArgs)
+		{
+			if (OnError != null)
+			{
+				Invoke(OnError, this, eventArgs);
+			}
+		}
+
+		private void RiseErrorEvent(OpenGL.OpenGLException throwingException)
+		{
+			ErrorEventArgs eventArgs = new ErrorEventArgs(	throwingException.LastWin32Error,
+															throwingException.OpenGLErrors,
+															throwingException.Message,
+															throwingException.StackTrace);
+			RiseErrorEvent(eventArgs);
+		}
+
 		#endregion
 	}
 }
